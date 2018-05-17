73a3c357b1f890275fa100b43d0b5ea6b1ddf321
GEM58 <gregemuller@gmail.com>
2018-05-16 22:41:27 -0500

    In KS- GIT cmd button -Commit and Push to sync KS proj w/ GitHub Repo

--- Scripts/Test 3 for grins/Script1526400968426.groovy
+++ Scripts/Test 3 for grins/Script1526400968426.groovy
@@ -19,7 +19,7 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.openBrowser('lifecharger.org')
+WebUI.openBrowser('http://opensource.demo.orangehrmlive.com/')
 
 WebUI.closeBrowser()
 
---
+++ Test Suites/New Test Suite1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-05-16T20:05:59</lastRun>
+   <mailRecipient>gregemuller@hotmail.com;</mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c6efcb35-1988-45a9-8e0a-b7beaa0a4d5e</testSuiteGuid>
+   <testCaseLink>
+      <guid>292b4734-39cd-4dba-884b-23e882ce7cb7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 1 by Tom</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
