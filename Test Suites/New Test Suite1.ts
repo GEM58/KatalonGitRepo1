d29e4e242e85a06f3ef203caaad3cfcf0dd2dbaf
GEM58 <gregemuller@gmail.com>
2018-05-18 12:18:09 -0500

    test message

--- Test Suites/New Test Suite1.ts
+++ Test Suites/New Test Suite1.ts
@@ -4,7 +4,7 @@
    <name>New Test Suite1</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-05-18T12:05:39</lastRun>
+   <lastRun>2018-05-18T12:17:35</lastRun>
    <mailRecipient>gregemuller@hotmail.com;</mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
